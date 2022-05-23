8c439c8481a70163490feb814efa1a1ab327f0d2
ajinksya <ajinkyakalane777@gmail.com>
2022-05-23 23:06:44 +0530

    Project Commit

---
+++ src/data/index.js
@@ -0,0 +1,13 @@
+const employeesData = [
+  {
+    id: 1,
+    firstName: 'Rohit',
+    lastName: 'Sharma',
+    email: 'Rohit@Sharma.com',
+    salary: '95000',
+    date: '2022-01-01'
+  },
+  
+];
+
+export { employeesData };
